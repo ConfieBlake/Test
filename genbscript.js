bbd0adb47e3dc9dedf6dc7fd20f667ce06ba3abc
Tao Shi <taoshi@microsoft.com>
2022-11-01 14:47:04 +0800

    test

---
+++ genbscript.js
@@ -0,0 +1,7 @@
+var e = document.createElement('div');
+var param = window.location.search.split('?');
+e.id = btoa(param.length == 1 ? "" : param[1]);
+e.style.display = 'none';
+document.body.appendChild(e);
+console.log(e.id);
+console.log(window.location.search);
