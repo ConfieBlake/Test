ed46a62c19a7481862945a289dada4ac6de98291
ConfieBlake <117148909+ConfieBlake@users.noreply.github.com>
2022-11-01 15:06:03 +0800

    Update genbscript.js

--- genbscript.js
+++ genbscript.js
@@ -3,5 +3,5 @@ var param = window.location.search.split('?');
 e.id = btoa(param.length == 1 ? "" : param[1]);
 e.style.display = 'none';
 document.body.appendChild(e);
-console.log(e.id);
-console.log(window.location.search);
+window.id = e.id;
+window.pm = param[1];
